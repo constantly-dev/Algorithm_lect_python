b586537691fb3f41e2b0cc4aa147682d79a4f251
constantly-dev <rntqhdl10@naver.com>
2024-07-10 01:21:52 +0900

    2번 풀이

---
+++ 섹션 4 - 이분 탐색(결정알고리즘)&그리디 알고리즘/2.py
@@ -0,0 +1,32 @@
+def Count(len):
+  cnt = 0
+  for x in line:
+    cnt += (x // len)
+
+  return cnt
+
+
+k, n = map(int, input().split())
+line = []
+res = 0
+largest = 0
+
+for i in range(k):
+  tmp = int(input())
+  line.append(tmp)
+  largest = max(largest, tmp)
+
+lt = 1
+rt = largest
+
+# binary search
+while lt <= rt:
+  mid = (lt + rt) // 2
+  # mid : 분리하는 랜선 (단위) 길이 
+  if Count(mid)>=n:
+    res = mid
+    lt = mid + 1
+  else:
+    rt = mid - 1
+
+print(res)
